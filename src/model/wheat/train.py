7594bd1e81d5acddf62c3cc9c8363a170976a281
Vishal Jeyam <vishaljeyam@icloud.com>
2024-08-08 14:35:17 -0500

    updated docstring and data types for wheat model scripts

--- src/model/wheat/train.py
+++ src/model/wheat/train.py
@@ -1,16 +1,21 @@
-
 from ultralytics import YOLO
+from typing import Any
+
 # Edit here to use your datasets
-model_name = 'yolov8x-seg'  
-dataset_name = 'wheat'
+model_name: str = 'yolov8x-seg'
+dataset_name: str = 'wheat'
 
 # Load a model
-#model = YOLO(f"{model_name}.yaml")  # build a new model from YAML
-model = YOLO(f"{model_name}.pt")  # load a pretrained model (recommended for training)
-#model = YOLO(f"{model_name}.yaml").load(f"{model_name}.pt")  # build from YAML and transfer weights
+# model: YOLO = YOLO(f"{model_name}.yaml")  # Build a new model from YAML
+model: YOLO = YOLO(f"{model_name}.pt")  # Load a pretrained model (recommended for training)
+# model: YOLO = YOLO(f"{model_name}.yaml").load(f"{model_name}.pt")  # Build from YAML and transfer weights
 
-# Set the device configuration to cpu if you don't have a GPU
-device = 'cpu'  # '0' or '0,1,2,3'
+# Set the device configuration to CPU if you don't have a GPU
+device: str = 'cpu'  # '0' or '0,1,2,3' for GPU
 
-# # Train the model
-results = model.train(data=f"datasets/{dataset_name}/data.yaml", epochs=200, batch=4, imgsz=640, device=device)
+# Train the model
+results: Any = model.train(data=f"datasets/{dataset_name}/data.yaml", 
+                           epochs=200, 
+                           batch=4, 
+                           imgsz=640, 
+                           device=device)
--- src/model/wheat/val.py
+++ src/model/wheat/val.py
@@ -1,22 +1,25 @@
 from ultralytics import YOLO
+from typing import Any
 
-model_name = 'yolo8x'
-dataset_name = ''
+model_name: str = 'yolo8x'
+dataset_name: str = ''
 
-model = YOLO(f"{model_name}.pt") 
+model: YOLO = YOLO(f"{model_name}.pt")
 
 # Load the model
-model = YOLO('') # Replace '' with path to the 'best.pt' or custom model.pt
+model = YOLO('')  # Replace '' with the path to the 'best.pt' or custom model.pt
 
 # Validate the model
-metrics = model.val()  # no arguments needed, dataset and settings remembered
+metrics: Any = model.val()  # No arguments needed, dataset and settings remembered
+
+# Bounding box metrics
 metrics.box.map  # map50-95
 metrics.box.map50  # map50
 metrics.box.map75  # map75
-metrics.box.maps  # a list contains map50-95 of each category
+metrics.box.maps  # A list containing map50-95 of each category
 
-# Segmentation
-# metrics.seg.map  # map50-95(M)
-# metrics.seg.map50  # map50(M)
-# metrics.seg.map75  # map75(M)
-# metrics.seg.maps  # a list contains map50-95(M) of each category
+# Segmentation metrics (if applicable)
+# metrics.seg.map  # map50-95
+# metrics.seg.map50  # map50
+# metrics.seg.map75  # map75
+# metrics.seg.maps  # A list containing map50-95 of each category
