06da2d35a30b3bd5ae2e49083834130e721792a1
Vishal Jeyam <vishaljeyam@icloud.com>
2024-08-08 14:31:11 -0500

    updated docstring and data types for panel model scripts

--- src/model/panel/test.py
+++ src/model/panel/test.py
@@ -1,16 +1,17 @@
 from ultralytics import YOLO
+from typing import List
 
 # Load a pretrained YOLOv8x model
-model = YOLO('yolo8x.pt') # Replace 'yolo8x.pt' with path to the 'best.pt'
+model: YOLO = YOLO('yolo8x.pt')  # Replace 'yolo8x.pt' with the path to 'best.pt'
 
 # Run inference with arguments
-result = model.predict('', save=True) # Save predictions to 'runs/detect/exp'
+result = model.predict('', save=True)  # Save predictions to 'runs/detect/exp'
 
 for results in result:
-    boxes = results.boxes
-    masks = result.masks
-    keypoints = result.keypoints
-    probs = result.probs
-    obb = result.obb
-    result.show()  # Show results
-    result.save()  # Save results
+    boxes = results.boxes  # List of detected bounding boxes
+    masks = result.masks  # Segmentation masks
+    keypoints = result.keypoints  # Keypoints detected in the image
+    probs = result.probs  # Class probabilities
+    obb = result.obb  # Oriented bounding boxes (if available)
+    results.show()  # Show results
+    results.save()  # Save results
--- src/model/panel/train.py
+++ src/model/panel/train.py
@@ -2,13 +2,13 @@ import os
 import torch
 from ultralytics import YOLO
 
-model_name = 'yolov8x'
-dataset_name = 'data'
+model_name: str = 'yolov8x'
+dataset_name: str = 'data'
 
 # Build the path to the data.yaml file relative to the script directory
-data_path = os.path.join(os.path.dirname(__file__), f"datasets/{dataset_name}/data.yaml")
-train_images_path = os.path.join(os.path.dirname(__file__), f"datasets/{dataset_name}/train/images")
-val_images_path = os.path.join(os.path.dirname(__file__), f"datasets/{dataset_name}/val/images")
+data_path: str = os.path.join(os.path.dirname(__file__), f"datasets/{dataset_name}/data.yaml")
+train_images_path: str = os.path.join(os.path.dirname(__file__), f"datasets/{dataset_name}/train/images")
+val_images_path: str = os.path.join(os.path.dirname(__file__), f"datasets/{dataset_name}/val/images")
 
 # Print paths for debugging
 print(f"Using data.yaml at: {data_path}")
@@ -16,10 +16,10 @@ print(f"Train images path: {train_images_path}")
 print(f"Validation images path: {val_images_path}")
 
 # Check if CUDA is available
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Load a model
-model = YOLO(f"{model_name}.pt")
+model: YOLO = YOLO(f"{model_name}.pt")
 
 # Train the model
 results = model.train(data=data_path,
