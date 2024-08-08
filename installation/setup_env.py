9ceabadda55997753f71933af4ae2bca4c8eeda5
Vishal Jeyam <vishaljeyam@icloud.com>
2024-08-08 14:25:08 -0500

    refactor setup_env.py for better documentation

--- installation/setup_env.py
+++ installation/setup_env.py
@@ -2,27 +2,47 @@ from venv import create
 import os
 from subprocess import run
 
-def check_requirements_file():
-    """Check if the requirements file exists in the current working directory."""
+def check_requirements_file() -> bool:
+    """Check if the requirements file exists in the current working directory.
+    
+    Returns:
+        bool: True if the requirements file exists, False otherwise.
+    """
     return os.path.exists(os.path.join(os.getcwd(), "installation", "requirements.txt"))
 
-def check_virtual_environment():
-    """Check if the virtual environment exists in the current working directory."""
+def check_virtual_environment() -> bool:
+    """Check if the virtual environment exists in the current working directory.
+    
+    Returns:
+        bool: True if the virtual environment exists, False otherwise.
+    """
     return os.path.exists("yolo8_venv")
 
-def get_virtual_environment_path():
-    """Get the path to the virtual environment."""
+def get_virtual_environment_path() -> str:
+    """Get the path to the virtual environment.
+    
+    Returns:
+        str: The path to the virtual environment.
+    """
     return os.path.join(os.getcwd(), "yolo8_venv")
 
-def create_virtual_environment():
-    """Create a virtual environment in the current working directory."""
-    venv_path = get_virtual_environment_path()
+def create_virtual_environment() -> bool:
+    """Create a virtual environment in the current working directory.
+    
+    Returns:
+        bool: True if the virtual environment is created successfully, False otherwise.
+    """
+    venv_path: str = get_virtual_environment_path()
     create(venv_path, with_pip=True)
     return check_virtual_environment()
 
-def install_requirements(venv_path):
-    """Install the requirements file in the virtual environment."""
-    python_executable = os.path.join(venv_path, "Scripts", "python.exe")
+def install_requirements(venv_path: str) -> None:
+    """Install the requirements from the requirements file into the virtual environment.
+    
+    Args:
+        venv_path (str): The path to the virtual environment where the requirements will be installed.
+    """
+    python_executable: str = os.path.join(venv_path, "Scripts", "python.exe")
     run([python_executable, "-m", "pip", "install", "-r", os.path.join("installation", "requirements.txt")])
 
 if __name__ == "__main__":
@@ -43,7 +63,7 @@ if __name__ == "__main__":
             print("Virtual environment not created")
             os.sys.exit(1)
 
-    venv_path = get_virtual_environment_path()
+    venv_path: str = get_virtual_environment_path()
     # Install the requirements
     install_requirements(venv_path)
     print("Requirements installed")
