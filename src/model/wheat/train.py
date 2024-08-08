5c53b21e28a73a6c576ea1598a54ad2d25a0c8b7
Vishal Jeyam <vishaljeyam@icloud.com>
2024-08-08 09:13:56 -0500

    updated training and validation scripts

--- src/model/wheat/train.py
+++ src/model/wheat/train.py
@@ -1,29 +1,16 @@
-import os
-import torch
-from ultralytics import YOLO
 
-model_name = 'yolov8x'
+from ultralytics import YOLO
+# Edit here to use your datasets
+model_name = 'yolov8x-seg'  
 dataset_name = 'wheat'
 
-# Build the path to the data.yaml file relative to the script directory
-data_path = os.path.join(os.path.dirname(__file__), f"datasets/{dataset_name}/data.yaml")
-train_images_path = os.path.join(os.path.dirname(__file__), f"datasets/{dataset_name}/train/images")
-val_images_path = os.path.join(os.path.dirname(__file__), f"datasets/{dataset_name}/val/images")
-
-# Print paths for debugging
-print(f"Using data.yaml at: {data_path}")
-print(f"Train images path: {train_images_path}")
-print(f"Validation images path: {val_images_path}")
-
-# Check if CUDA is available
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
 # Load a model
-model = YOLO(f"{model_name}.pt")
+#model = YOLO(f"{model_name}.yaml")  # build a new model from YAML
+model = YOLO(f"{model_name}.pt")  # load a pretrained model (recommended for training)
+#model = YOLO(f"{model_name}.yaml").load(f"{model_name}.pt")  # build from YAML and transfer weights
 
-# Train the model
-results = model.train(data=data_path,
-                      epochs=100,
-                      batch=4,
-                      imgsz=640,
-                      device=device)
+# Set the device configuration to cpu if you don't have a GPU
+device = 'cpu'  # '0' or '0,1,2,3'
+
+# # Train the model
+results = model.train(data=f"datasets/{dataset_name}/data.yaml", epochs=200, batch=4, imgsz=640, device=device)
--- src/model/wheat/val.py
+++ src/model/wheat/val.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-model_name = 'yolo8x-seg'
+model_name = 'yolo8x'
 dataset_name = ''
 
-model = YOLO(f"{model_name}.pt")
+model = YOLO(f"{model_name}.pt") 
 
 # Load the model
 model = YOLO('') # Replace '' with path to the 'best.pt' or custom model.pt
@@ -16,7 +16,7 @@ metrics.box.map75  # map75
 metrics.box.maps  # a list contains map50-95 of each category
 
 # Segmentation
-metrics.seg.map  # map50-95
-metrics.seg.map50  # map50
-metrics.seg.map75  # map75
-metrics.seg.maps  # a list contains map50-95 of each category
+# metrics.seg.map  # map50-95(M)
+# metrics.seg.map50  # map50(M)
+# metrics.seg.map75  # map75(M)
+# metrics.seg.maps  # a list contains map50-95(M) of each category
